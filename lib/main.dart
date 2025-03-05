547db73b9fce0368cb66f696a98586e5023b3d06
Savinu Gunaratne <business.savinu@gmail.com>
2025-03-05 15:51:47 +0530

    an auth page to track user logins

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/firebase_options.dart';
+import 'package:frontend/pages/auth_page.dart';
 //import 'package:frontend/pages/intro_page.dart';
 import 'package:frontend/pages/login_page.dart';
 
@@ -15,6 +16,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: AuthPage());
   }
 }
---
+++ lib/pages/preferences_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PreferencesPage extends StatelessWidget {
+  const PreferencesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Preferences Page")));
+  }
+}
