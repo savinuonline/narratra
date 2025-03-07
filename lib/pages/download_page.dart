f06e72510d9d359f008640ca039de896c4979c7e
Geilee Gehara <dvggehara@gmail.com>
2025-03-07 21:41:19 +0530

    add sub setting2

--- lib/pages/download_page.dart
+++ lib/pages/download_page.dart
@@ -58,6 +58,48 @@ class DownloadPage extends StatelessWidget {
                 ],
             ),
           ),
+
+          const SizedBox(height: 20,),
+          SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: const Color.fromARGB(255, 180, 209, 249),
+                  ),
+                  child: Icon(Ionicons.book_outline, size: 26,
+                  color: const Color.fromARGB(255, 29, 14, 172),),
+                
+                ),
+                SizedBox(width: 12),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Offline Audio Book", 
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    )),
+                  ],
+                ),
+                const Spacer(),
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 227, 227, 227),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Icon(Ionicons.chevron_forward_outline),
+                )
+                ],
+            ),
+          ),
+
       ]
       
     );
