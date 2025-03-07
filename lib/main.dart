455ef6ebc2afa743d4ee15aa42823fedd7442847
Mashinee <mashinee.20232574@iit.ac.lk>
2025-03-08 00:44:09 +0530

    created a layout for profile page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/firebase_options.dart';
 import 'package:frontend/pages/intro_page.dart';
+import 'package:frontend/pages/profile_page.dart';
+import 'package:frontend/widgets/custom_bottom_nav_bar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,9 +16,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: IntroPage(),
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MainScreen());
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 4;
+
+  final List<Widget> _pages = [
+    Center(child: Text("Explore Page")),
+    Center(child: Text("Search Page")),
+    Center(child: Text("Library Page")),
+    Center(child: Text("Play Page")),
+    const ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: CustomBottomNavBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          "My Profile",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(child: Text("", style: TextStyle(fontSize: 20))),
+    );
+  }
+}
