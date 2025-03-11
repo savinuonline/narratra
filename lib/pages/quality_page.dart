40f7667846891f0171d6ea0113763dae6a131aea
Geilee Gehara <dvggehara@gmail.com>
2025-03-11 20:20:31 +0530

    add audio qualities to download quality

--- lib/pages/quality_page.dart
+++ lib/pages/quality_page.dart
@@ -22,6 +22,66 @@ class QualityPage extends StatelessWidget {
             ),
         ),
       ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16.0), 
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 10),
+
+            Column(
+              children: [
+                _buildHeaderRow(),
+                const Divider(),
+                _buildDataRow("360p", "600MB"),
+                _buildDataRow("480p", "1.2GB"),
+                _buildDataRow("720p", "2.4GB"),
+                 // Separates header from data rows
+                
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
+
+  Widget _buildHeaderRow(){
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildCell("Audio Quality", isHeader: true),
+          _buildCell("File Size", isHeader: true),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDataRow(String quality, String size){
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildCell(quality),
+          _buildCell(size),
+        ],
+      ) 
+    );
+  }  
+  
+  Widget _buildCell(String text, {bool isHeader = false}){
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 18,
+        fontWeight: isHeader ? FontWeight.bold : FontWeight.normal,
+      ),
+    );
+  }
+
+
 }
