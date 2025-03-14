d7178316703b56afc75724882b90f0caec94b06d
Savinu Gunaratne <business.savinu@gmail.com>
2025-03-14 17:14:40 +0530

    google login/ios/ auth_service/ added GestureDetector to the squre_tile  component

--- lib/components/squre_tile.dart
+++ lib/components/squre_tile.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 
 class SqureTile extends StatelessWidget {
   final String imagePath;
-  const SqureTile({super.key, required this.imagePath});
+  final Function()? onTap;
+  const SqureTile({super.key, required this.imagePath, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(5),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.white),
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.grey[200],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.grey[200],
+        ),
+        child: Image.asset(imagePath, height: 50),
       ),
-      child: Image.asset(imagePath, height: 50),
     );
   }
 }
