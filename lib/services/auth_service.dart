14588de096c8bfb048101cdd9b27a39d18007a6c
Savinu Gunaratne <business.savinu@gmail.com>
2025-03-14 17:02:03 +0530

    google login/ios/ created auth_service.dart to setup google sign in

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,12 @@
+class AuthService {
+  //Google Sign In
+  signInWithGoogle() async {
+    //Begin interactive signIn process
+
+    //Obtain auth details from request
+
+    //Create a new credential for the user
+
+    //Sign in the user with the credential
+  }
+}
