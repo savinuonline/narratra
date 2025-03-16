4356b382da54f5a69ea5d03a4726f8f3b2afe8e0
Savinu Gunaratne <business.savinu@gmail.com>
2025-03-16 17:05:15 +0530

    feature/search/ui/starting point

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/firebase_options.dart';
 import 'package:frontend/pages/auth_page.dart';
+import 'package:frontend/pages/search_page.dart';
 //import 'package:frontend/pages/intro_page.dart';
 
 void main() async {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+      home: SearchPage(),
     );
   }
 }
