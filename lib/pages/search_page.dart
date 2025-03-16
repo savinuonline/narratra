79d06438f0a4b7208c4540b03602e6568ccf747f
Savinu Gunaratne <business.savinu@gmail.com>
2025-03-16 17:29:07 +0530

    feature/search/ui/added menu icon and customized

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          //App Bar
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Container(
+              height: 50,
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.grey[200],
+              ),
+              child: Image.asset(
+                'lib/images/menu.png',
+                height: 100,
+                color: Colors.grey[800],
+              ),
+            ),
+          ),
+
+          SizedBox(height: 50),
+
+          //Discover a New Story
+
+          //Search Bar
+
+          //for you
+
+          //recently added
+        ],
+      ),
+    );
   }
 }
