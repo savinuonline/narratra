5ac84b6d485607b3a57eb60cf3a07383c61c992f
Shakeel Peterson <denam.20230173@iit.ac.lk>
2025-03-16 20:28:17 +0530

    Change the maind method correct the runnable interface on the device

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/pages/intro_page.dart';
+import 'package:frontend/pages/Subscription.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: IntroPage(),
+      home: SubscriptionPage(),
     );
   }
 }
