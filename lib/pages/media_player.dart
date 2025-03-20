9bb5429cedb03b08e3d2df41d445a183fe559cc3
Mashinee <mashinee.20232574@iit.ac.lk>
2025-03-20 12:37:27 +0530

    added text label to the top

--- lib/pages/media_player.dart
+++ lib/pages/media_player.dart
@@ -13,14 +13,31 @@ class MediaPlayerPage extends StatelessWidget {
               Color.fromARGB(255, 3, 21, 46), // Dark Blue (Top)
               Color.fromARGB(255, 202, 218, 234), // Light Blue (Bottom)
             ],
-            stops: [0.3, 1.5],
+            stops: [0.3, 1.0], // Adjusted for smoother transition
           ),
         ),
-        child: Center(
-          child: Text(
-            " media player page",
-            style: TextStyle(fontSize: 22, color: Colors.white),
-          ),
+        child: Column(
+          children: [
+            SizedBox(height: 40), // Space for status bar
+            Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                "PLAYING NOW",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: const Color.fromARGB(255, 234, 234, 240),
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5,
+                ),
+              ),
+            ),
+            Spacer(), // Pushes content to center
+            Text(
+              "Media Player Page",
+              style: TextStyle(fontSize: 22, color: Colors.white),
+            ),
+            SizedBox(height: 20),
+          ],
         ),
       ),
     );
