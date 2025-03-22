c93b7b6acb2439bda120f41a32b9406f3514ed4b
Kithmina Kaluarachchi <kithmina.20230197@iit.ac.lk>
2025-03-22 17:59:06 +0530

    Enhance UI components: increase button font size, update app bar color, and add gradient background and custom refresh indicator widgets.

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -23,7 +23,7 @@ class MyButton extends StatelessWidget {
             style: const TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
-              fontSize: 16,
+              fontSize: 18,
             ),
           ),
         ),
