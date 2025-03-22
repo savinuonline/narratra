76a27fdf422a6c1af1a460d7015654499416f82a
Geilee Gehara <dvggehara@gmail.com>
2025-03-22 12:32:58 +0530

    add add account page and update email page

---
+++ lib/pages/add_account.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddAccount extends StatefulWidget {
+  const AddAccount({super.key});
+
+  @override
+  State<AddAccount> createState() => _AddAccountState();
+}
+
+class _AddAccountState extends State<AddAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/update_mali.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdateMali extends StatefulWidget {
+  const UpdateMali({super.key});
+
+  @override
+  State<UpdateMali> createState() => _UpdateMaliState();
+}
+
+class _UpdateMaliState extends State<UpdateMali> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
