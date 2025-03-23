36ec14475a546fac04afcd40698fb1f0fff1ff6a
Shakeel Peterson <denam.20230173@iit.ac.lk>
2025-03-23 16:39:38 +0530

    Recreating the subscription page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/pages/Subscription.dart';
+import 'package:frontend/pages/subscription.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SubscriptionPage(),
+      home: subscription(),
     );
   }
 }
