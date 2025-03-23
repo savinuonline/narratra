fbf65ea72699d7e655e25d97056789ddcbf12aad
Savinu Gunaratne <business.savinu@gmail.com>
2025-03-23 10:22:57 +0530

    recently added/ finished UI design

--- lib/components/recent_book_card.dart
+++ lib/components/recent_book_card.dart
@@ -17,14 +17,64 @@ class RecentBookCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Container(height: 40, child: Image.asset(bookImagePath)),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Container(
+                    height: 85,
+                    padding: EdgeInsets.all(5),
+                    color: Colors.grey,
+                    child: Image.asset(bookImagePath),
+                  ),
+                ),
 
-          Column(children: [Text(bookName), Text(authorName)]),
-          Text(rating.toString()),
-        ],
+                SizedBox(width: 10),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      bookName,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    SizedBox(height: 6),
+
+                    Text(authorName, style: TextStyle(color: Colors.grey[600])),
+                  ],
+                ),
+              ],
+            ),
+
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Container(
+                padding: EdgeInsets.all(5),
+                color: Colors.amber,
+                child: Text(
+                  rating.toString(),
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
