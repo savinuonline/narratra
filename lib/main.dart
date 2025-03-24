9e11d84e95a715cdd3207d03dbc18e4e30d7d018
Senuu01 <senuthdamjaya55@gmail.com>
2025-03-24 14:13:09 +0530

    Update `main.dart` to include the profile page.

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:frontend/pages/login_page.dart';
 import 'package:frontend/pages/register_page.dart';
 import 'package:frontend/pages/splash_screen.dart';
 import 'package:frontend/pages/media_player.dart';
+
 import 'package:frontend/pages/profile_page.dart';
 
 Future<void> main() async {
